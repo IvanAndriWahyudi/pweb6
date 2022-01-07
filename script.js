293c172577237f60f0d8098a4907ef83ec12b30c
IvanAndriWahyudi <ivanandri.w04@gmail.com>
2022-01-07 15:22:52 +0700

    switch case

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-//if/else
-var angka = "15";
-console.log("angka = " + angka);
-if (angka % 2 == 0) {
-  console.log(angka + " adalah ganjil");
-} else {
-  console.log(angka + " adalah genap");
+//switch case
+var input = prompt("pilih 1.if/else, 2.Ternary operator, 3.For Loop, 4.For in loop, 5.For off Loop, 6.while");
+switch (input) {
+  case "1":
+    //if/else
+    var angka = "15";
+    console.log("angka = " + angka);
+    if (angka % 2 == 0) {
+      console.log(angka + " adalah ganjil");
+    } else {
+      console.log(angka + " adalah genap");
+    }
+    break;
+
+  case "2":
+    break;
+
+  case "3":
+    break;
+
+  case "4":
+    break;
+
+  case "5":
+    break;
+
+  case "6":
+    break;
 }
