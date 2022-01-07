7d33a58e75cf73dc369d01e6c2aaa91262d4d595
IvanAndriWahyudi <ivanandri.w04@gmail.com>
2022-01-07 15:29:36 +0700

    For In Loop

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var input = prompt("pilih 1.if/else, 2.Ternary operator, 3.For Loop, 4.For in lo
 switch (input) {
   case "1":
     //if/else
+    console.log("IF/ELSE");
     var angka = "15";
     console.log("angka = " + angka);
     if (angka % 2 == 0) {
@@ -14,6 +15,7 @@ switch (input) {
 
   case "2":
     //Ternary operator
+    console.log("TERNARY OPERATOR");
     var tebakangka = prompt("Masukan Tebakan Angka 1-3 = ");
     var tebak = tebakangka == 2 ? "Benar" : "Salah";
     console.log("Tebakan Anda " + tebak);
@@ -21,6 +23,7 @@ switch (input) {
 
   case "3":
     //forloop
+    console.log("FOOR LOOP");
     var kelipatan = prompt("Masukan Angka Kelipatan = ");
     var i;
     for (i = 1; i <= 20; i++) {
@@ -29,6 +32,18 @@ switch (input) {
     break;
 
   case "4":
+    //for in loop
+    console.log("FOR IN LOOP");
+    let mobil = {
+      namaPemilik: "Ivan",
+      model: "Honda Civic",
+      type: "Sport",
+      warna: "Putih",
+      noPolosi: "BM2322GD",
+    };
+    for (let a in mobil) {
+      console.log(a, ":", mobil[a]);
+    }
     break;
 
   case "5":
